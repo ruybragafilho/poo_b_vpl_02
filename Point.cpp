55f096c9f4cb729e890930528caa04f6a0fd3aa5
ruybragafilho <ruybragafilho@gmail.com>
2021-03-04 22:31:26 -0300

    Versao Final

--- Point.cpp
+++ Point.cpp
@@ -4,16 +4,24 @@
 
 std::ostream & operator << (std::ostream &out, const Point &p) {
   // TODO: implement this operator.
+  return out << "(" << p.x << ", "<< p.y << ")";   
 }
 
 std::istream & operator >> (std::istream &in,  Point &p) {
   // TODO: implement this operator.
+  in >> p.x >> p.y;
+  return in;
 }
 
 Point Point::operator + (const Point &p) {
   // TODO: implement this operator.
+  return Point( this->x + p.x, 
+                this->y + p.y );
 }
 
 Point& Point::operator += (const Point &p) {
   // TODO: implement this operator.
+  this->x += p.x;
+  this->y += p.y;
+  return *this;
 }
